1520f98e35950d057c2e82ee7636ec46f43d5db0
PeriodGamingYT <nathanryanphillips@gmail.com>
2023-08-07 14:30:22 -0600

    add two character operations

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum { NUM, STOP };
+enum { NUM, STOP, IS_EQ, IS_MORE_EQ, IS_LESS_EQ, IS_NOT_EQ, SHL, SHR };
 int token = 0;
 int token_val = 0;
 char *src = NULL;
@@ -16,7 +16,21 @@ void next() {
 		src++;
 	}
 
-	// for opers
+	const int new_token[] = { IS_EQ, IS_MORE_EQ, IS_LESS_EQ, IS_NOT_EQ, SHL, SHR };
+	const char *two_char_opers[] = {
+		"=><!<>",
+		"====<>"
+	};
+	
+	for(int i = 0; two_char_opers[0][i] != 0; i++) {
+		if(*src == two_char_opers[0][i] && src[1] == two_char_opers[1][i]) {
+			token = new_token[i];
+			src += 2;
+			return;
+		}
+	}
+
+	// for one char opers
 	token = *(src++);
 	if(token >= '0' && token <= '9') {
 		token_val = token - '0';
@@ -59,8 +73,29 @@ int expr_tail(int left_val) {
 		case '+': expect('+'); return expr_tail(left_val + value());
 		case '-': expect('-'); return expr_tail(left_val - value());
 		case '*': expect('*'); return expr_tail(left_val * value());
-		case '/': expect('/'); return expr_tail(left_val / value());
+		case '/': 
+			expect('/'); 
+			int right_val = value();
+			if(right_val == 0) {
+				fprintf(stderr, "can't divide %d by 0\n", left_val);
+				exit(1);
+			}
+			
+			return expr_tail(left_val / right_val);
+			break;
+		
 		case '%': expect('%'); return expr_tail(left_val % value());
+		case '<': expect('<'); return expr_tail(left_val < value());
+		case '>': expect('>'); return expr_tail(left_val > value());
+		case '&': expect('&'); return expr_tail(left_val & value());
+		case '|': expect('|'); return expr_tail(left_val | value());
+		case ':': expect(':'); return expr_tail(left_val ^ value());
+		case IS_EQ: expect(IS_EQ); return expr_tail(left_val == value());
+		case IS_MORE_EQ: expect(IS_MORE_EQ); return expr_tail(left_val >= value());
+		case IS_LESS_EQ: expect(IS_LESS_EQ); return expr_tail(left_val <= value());
+		case IS_NOT_EQ: expect(IS_NOT_EQ); return expr_tail(left_val != value());
+		case SHL: expect(SHL); return expr_tail(left_val << value());
+		case SHR: expect(SHR); return expr_tail(left_val >> value());
 	}
 
 	return left_val;
@@ -93,7 +128,7 @@ int main() {
 		src = buffer_ptr;
 		next();
 		while(token != STOP && *src && *src != -1) {
-			printf("%d \n", expr());
+			printf("%d\n", expr());
 		}
 
 		memset(buffer, 0, sizeof(char) * BUFFER_MAX);
